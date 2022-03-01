29b41d71da3817d78e68f3cf0e4f5215537c1367
lrodenyoder <rodenlau000@gmail.com>
2022-03-01 16:03:33 -0600

    github user test pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,11 @@
 const Engineer = require('../lib/Engineer.js');
 
 test('create engineer object', () => {
-    const engineer = new Engineer("Dave", "0", "dave@gmail.com", "24", "gituser");
+    const engineer = new Engineer("Dave", "0", "dave@gmail.com", "24", "git user");
     
     expect(engineer.name).toBe("Dave");
     expect(engineer.id).toEqual(expect.any(Number));
     expect(engineer.email).toBe("dave@gmail.com");
+
+    expect(engineer.github).toBe('git user');
 });
