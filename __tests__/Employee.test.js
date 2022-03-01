6332f40492a754c16780ba4502e96f27fb4e5f89
lrodenyoder <rodenlau000@gmail.com>
2022-03-01 14:27:48 -0600

    id test pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,8 @@
 const Employee = require("../lib/Employee.js");
 
 test("creates employee object", () => {
-  const employee = new Employee("Dave");
+  const employee = new Employee("Dave", "0", 'blank.email');
 
-  expect(employee.name).toBe("Dave");
+    expect(employee.name).toBe("Dave");
+    expect(employee.id).toEqual(expect.any(Number));
 });
