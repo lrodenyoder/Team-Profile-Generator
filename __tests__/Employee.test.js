8a5522e54900fc9ebddbf03b78029433cf9c3791
lrodenyoder <rodenlau000@gmail.com>
2022-03-01 14:29:11 -0600

    email test pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,9 @@
 const Employee = require("../lib/Employee.js");
 
 test("creates employee object", () => {
-  const employee = new Employee("Dave", "0", 'blank.email');
+  const employee = new Employee("Dave", "0", 'dave@gmail.com');
 
     expect(employee.name).toBe("Dave");
     expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe("dave@gmail.com");
 });
