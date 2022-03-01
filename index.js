f1a44909872e40a81316ed9b6398178c79591480
lrodenyoder <rodenlau000@gmail.com>
2022-03-01 17:28:15 -0600

    create prompts for manager info and employee info

--- index.js
+++ index.js
@@ -2,24 +2,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./src/generateHTML.js');
-//prompted questions to get user input
-const questions = [
-    {
-        type: 'input',
-        name: 'manager-name',
-        message: "What is the team manager's name?",
-        validate: (nameInput) => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter a name!");
-                return false;
-            }
-        }
-    },
-];
+const Employee = require("./lib/Employee.js");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 
 //FUNCTIONS
+const promptManager = () => {
+
+    console.log(`
+
+    Generate a Team Profile
+
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a name!");
+                    return false;
+                }
+            }
+        },
+        
+    ]);
+};
+
+const promptEmployee = employeeData => {
+    console.log(`
+        Add a New Employee
+    `)
+
+    return inquirer.prompt([
+      {
+        type: "list",
+        name: "employeeType",
+        message: "Please select and employee type to add",
+        choices: ["Engineer", "Intern"],
+      },
+    ]);
+};
+
 //creates HTML file from user input data
 const writeToFile = (data) => {
     return new Promise((resolve, reject) => {
@@ -37,20 +66,13 @@ const writeToFile = (data) => {
     });
 };
 
-//initialize app
-const init = (htmlData) => {
-    console.log(`
-    Generate a Team Profile
-    `);
-
-    return inquirer.prompt(questions);
-};
-
 //call to initialize app
-init()
-    // .then((data) => {
-    //     return generateHTML(data);
-    // })
+promptManager()
+    .then(promptEmployee)
+    .then((data) => {
+        console.log(data);
+        return generateHTML(data);
+    })
     // .then((htmlData) => {
     //     return writeToFile(htmlData);
     // })
---
+++ src/generateHTML.js
@@ -0,0 +1,14 @@
+const Employee = require('../lib/Employee.js');
+const Manager = require('../lib/Manager.js');
+const Engineer = require('../lib/Engineer.js');
+const Intern = require('../lib/Intern.js');
+
+
+function generateHTML(data) {
+    return Employee, Manager
+};
+
+
+
+
+module.exports = generateHTML;
