2f7487db51b6cc8196b818a2852a2cb16ec7a89b
lrodenyoder <rodenlau000@gmail.com>
2022-03-01 18:09:25 -0600

    created object for prompts logic

--- index.js
+++ index.js
@@ -1,84 +1,25 @@
 //VARIABLES
-const inquirer = require('inquirer');
-const fs = require('fs');
+const Prompt = require('./lib/Prompt');
 const generateHTML = require('./src/generateHTML.js');
-const Employee = require("./lib/Employee.js");
-const Manager = require("./lib/Manager.js");
-const Engineer = require("./lib/Engineer.js");
-const Intern = require("./lib/Intern.js");
 
-//FUNCTIONS
-const promptManager = () => {
+new Prompt().promptManager();
 
-    console.log(`
 
-    Generate a Team Profile
 
-    `);
-
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: "What is the team manager's name?",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please enter a name!");
-                    return false;
-                }
-            }
-        },
-        
-    ]);
-};
-
-const promptEmployee = employeeData => {
-    console.log(`
-        Add a New Employee
-    `)
-
-    return inquirer.prompt([
-      {
-        type: "list",
-        name: "employeeType",
-        message: "Please select and employee type to add",
-        choices: ["Engineer", "Intern"],
-      },
-    ]);
-};
-
-//creates HTML file from user input data
-const writeToFile = (data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('.dist/index.html', data, (err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            resolve({
-                ok: true,
-                message: 'File Created!'
-            });
-        });
-    });
-};
 
 //call to initialize app
-promptManager()
-    .then(promptEmployee)
-    .then((data) => {
-        console.log(data);
-        return generateHTML(data);
-    })
-    // .then((htmlData) => {
-    //     return writeToFile(htmlData);
-    // })
-    // .then((writeFileResponse) => {
-    //     console.log(writeFileResponse);
-    // })
-    .catch((err) => {
-        console.error(err);
-    });
+// promptManager()
+//     .then(promptEmployee)
+//     .then((data) => {
+//         console.log(data);
+//         return generateHTML(data);
+//     })
+//     // .then((htmlData) => {
+//     //     return writeToFile(htmlData);
+//     // })
+//     // .then((writeFileResponse) => {
+//     //     console.log(writeFileResponse);
+//     // })
+//     .catch((err) => {
+//         console.error(err);
+//     });
