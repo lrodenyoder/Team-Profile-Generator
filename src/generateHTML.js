5b929116a56be219827f2cb384335f4081b49996
lrodenyoder <rodenlau000@gmail.com>
2022-03-05 15:14:21 -0600

    email and github link out

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,7 +3,7 @@ const Manager = require("../lib/Manager.js");
 const Engineer = require("../lib/Engineer.js");
 const Intern = require("../lib/Intern.js");
 
-const generateManager = managerData =>{
+const generateManager = (managerData) => {
   return `<div class="card">
             <h1 class="card-header">
                 ${managerData.getName()} <br>
@@ -11,55 +11,58 @@ const generateManager = managerData =>{
             </h1>
             <div class="card-body">
                 <p class="card-text">ID: ${managerData.getId()}</p>
-                <p class="card-text">Email: ${managerData.getEmail()}</p>
-                <p class="card-text">Office Number: ${managerData.officeNumber}</p>
+                <p class="card-text">Email: <a href = "mailto: ${managerData.getEmail()}">${managerData.getEmail()}</a></p>
+                <p class="card-text">Office Number: ${
+                  managerData.officeNumber
+                }</p>
             </div>
         </div>`;
 };
 
-const generateEmployees = employeeData => {
-    console.log(employeeData);
-
-    return `
-    ${employeeData.filter(({ github }) => github)
-        .map(({ employeeType, name, id, email, github }) => {
-    return `<div class="card">
+const generateEmployees = (employeeData) => {
+  //uses .filter() to separate engineer and intern employees and .map() to generate cards for each employee
+  return `
+    ${employeeData
+      .filter(({ github }) => github)
+      .map(({ employeeType, name, id, email, github }) => {
+        return `<div class="card">
             <h1 class="card-header">
                 ${name}<br>
                 <i class="bi bi-display"></i> ${employeeType}
             </h1>
             <div class="card-body">
                 <p class="card-text">ID: ${id}</p>
-                <p class="card-text">Email: ${email}</p>
-                <p class="card-text">GitHub: ${github}</p>
+                <p class="card-text">Email: <a href = "mailto: ${email}">${email}<a></p>
+                <p class="card-text">GitHub: <a href = "https://github.com/${github}" target = "_blank">${github}</a></p>
             </div>
         </div>
-    `
-        })}
-    ${employeeData.filter(({ school }) => school)
-        .map(({ employeeType, name, id, email, school }) => {
-    return `<div class="card">
+    `;
+      })}
+
+    ${employeeData
+      .filter(({ school }) => school)
+      .map(({ employeeType, name, id, email, school }) => {
+        return `<div class="card">
             <h1 class="card-header">
                 ${name}<br>
                 <i class="bi bi-mortarboard"></i> ${employeeType}
             </h1>
             <div class="card-body">
                 <p class="card-text">ID: ${id}</p>
-                <p class="card-text">Email: ${email}</p>
+                <p class="card-text">Email: <a href = "mailto: ${email}">${email}</a></p>
                 <p class="card-text">School: ${school}</p>
             </div>
         </div>
-    `
-        })}
+    `;
+      })}
 
-`
-}
+`;
+};
 
 function generateHTML(answers) {
-  //console.log(answers);
   const { name, id, email, officeNumber, ...employeesObject } = answers;
-    let manager = new Manager(name, id, email, officeNumber);
-    //extracts employee array from answers object
+  let manager = new Manager(name, id, email, officeNumber);
+  //extracts employee array from answers object
   let employeeArray = employeesObject.employees;
 
   return `<!DOCTYPE html>
@@ -82,9 +85,9 @@ function generateHTML(answers) {
     </header>
 
         <main class="card-deck">
-        ${generateManager(manager)}
-        ${generateEmployees(employeeArray)}
-    </main>
+            ${generateManager(manager)}
+            ${generateEmployees(employeeArray)}
+        </main>
 
 </body>
 
